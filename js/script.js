5d0857ae533c20de4e7186db0aec5845043ad085
Joe <joe.vieira@aluno.senai.br>
2026-08-21 14:00:31 -0300

    hahahaha

--- js/script.js
+++ js/script.js
@@ -1,21 +1,92 @@
 
 
-let pergunta = ["Quando você precisa explicar uma tarefa complexa para alguém que não domina o assunto, como costuma fazer para garantir que a pessoa compreenda?"
-    , "Conte sobre uma situação em que precisou trabalhar com alguém que tinha uma opinião ou forma de trabalhar muito diferente da sua. Como lidou com isso?",
-     "Se você perceber um conflito entre dois colegas que está prejudicando o trabalho da equipe, qual seria sua atitude?",
-    
-    
-    
-    
-    ]
+let pergunta = [
+    "Como você reage quando recebe uma crítica?",
+    "Como você lida com trabalhos em equipe?",
+    "O que você faz quando surge um conflito?",
+    "Como você reage diante de uma tarefa difícil?",
+    "Como você organiza suas tarefas?",
+    "Como você lida com mudanças?",
+    "Como você se comunica com outras pessoas?",
+    "Como você reage quando comete um erro?",
+    "Como você lida com pressão?",
+    "Como você demonstra iniciativa?"
+];
+
+let resposta1 = [
+    "Aceito e melhoro",
+    "Colaboro bem",
+    "Busco dialogar",
+    "Busco soluções",
+    "Planejo tudo",
+    "Adapto-me rápido",
+    "Sou claro e objetivo",
+    "Aprendo com ele",
+    "Mantenho a calma",
+    "Procuro ajudar"
+];
+
+let resposta2 = [
+    "Penso sobre ela",
+    "Faço minha parte",
+    "Aguardo o momento certo",
+    "Tento aos poucos",
+    "Organizo quando preciso",
+    "Preciso de tempo",
+    "Tento me expressar",
+    "Tento corrigir",
+    "Faço o possível",
+    "Ajudo quando solicitado"
+];
+
+let resposta3 = [
+    "Fico na defensiva",
+    "Prefiro trabalhar sozinho",
+    "Evito o problema",
+    "Desisto facilmente",
+    "Deixo para depois",
+    "Resisto às mudanças",
+    "Tenho dificuldade",
+    "Culpo outras pessoas",
+    "Fico muito estressado",
+    "Espero receber ordens"
+];
+
+
 
 let perguntaID = document.getElementById('pergunta')
+let r1 = document.getElementById('R1')
+let r2 = document.getElementById('R2')
+let r3 = document.getElementById('R3')
+
+
 
 
 function questionario( IDpergunta){
 
-    IDpergunta = 0
-    perguntaID.innerHTML = pergunta[0]
+    
+    perguntaID.innerHTML = pergunta[IDpergunta]
+    r1.innerHTML = resposta1[IDpergunta]
+    r2.innerHTML = resposta2[IDpergunta]
+    r3.innerHTML = resposta3[IDpergunta]
 }
 
-questionario(0)
+
+let increment = 0
+
+
+function avancar(){
+
+questionario(increment)
+
+ increment++
+
+
+    
+if(increment>9){
+    increment = 0
+}
+ }
+
+
+
