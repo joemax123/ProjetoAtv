3619815f4d1f9b59ebe85683a37d66324c82af19
joemax123 <joe.vieira@aluno.senai.br>
2026-08-25 08:27:15 -0300

    bnfnnnhfh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 let pergunta = [
     "Como você reage quando recebe uma crítica?",
     "Como você lida com trabalhos em equipe?",
@@ -52,41 +50,114 @@ let resposta3 = [
     "Espero receber ordens"
 ];
 
+let perguntaID = document.getElementById("pergunta");
+let r1 = document.getElementById("R1");
+let r2 = document.getElementById("R2");
+let r3 = document.getElementById("R3");
+let resultado = document.getElementById("resultado");
 
+let increment = 0;
+let respostasSelecionadas = [];
 
-let perguntaID = document.getElementById('pergunta')
-let r1 = document.getElementById('R1')
-let r2 = document.getElementById('R2')
-let r3 = document.getElementById('R3')
+function questionario(idPergunta) {
+    perguntaID.innerHTML = pergunta[idPergunta];
 
+    r1.innerHTML = resposta1[idPergunta];
+    r2.innerHTML = resposta2[idPergunta];
+    r3.innerHTML = resposta3[idPergunta];
 
+    r1.style.display = "inline-block";
+    r2.style.display = "inline-block";
+    r3.style.display = "inline-block";
 
-
-function questionario( IDpergunta){
-
-    
-    perguntaID.innerHTML = pergunta[IDpergunta]
-    r1.innerHTML = resposta1[IDpergunta]
-    r2.innerHTML = resposta2[IDpergunta]
-    r3.innerHTML = resposta3[IDpergunta]
+    resultado.innerHTML = "";
 }
 
+r1.addEventListener("click", function () {
+    respostasSelecionadas[increment] = 2;
+    avancar();
+});
 
-let increment = 0
+r2.addEventListener("click", function () {
+    respostasSelecionadas[increment] = 1;
+    avancar();
+});
 
+r3.addEventListener("click", function () {
+    respostasSelecionadas[increment] = 0;
+    avancar();
+});
 
-function avancar(){
-
-questionario(increment)
-
- increment++
-
-
-    
-if(increment>9){
-    increment = 0
+function avancar() {
+    if (increment < pergunta.length - 1) {
+        increment++;
+        questionario(increment);
+    } else {
+        mostrarResultado();
+    }
 }
- }
 
+function voltar() {
+    if (increment > 0) {
+        increment--;
+        questionario(increment);
+    }
+}
 
+function calcularPontuacao() {
+    let pontuacao = 0;
 
+    for (let pontos of respostasSelecionadas) {
+        if (pontos !== undefined) {
+            pontuacao += pontos;
+        }
+    }
+
+    return pontuacao;
+}
+
+function mostrarResultado() {
+    let pontuacao = calcularPontuacao();
+
+    perguntaID.innerHTML = "Resultado final";
+
+    r1.style.display = "none";
+    r2.style.display = "none";
+    r3.style.display = "none";
+
+    if (pontuacao >= 16) {
+        resultado.innerHTML = `
+            <div class="azul">
+                🔵 <strong>Acima da expectativa</strong>
+                <br>
+                Pontuação: ${pontuacao}/20
+            </div>
+        `;
+    } else if (pontuacao >= 11) {
+        resultado.innerHTML = `
+            <div class="verde">
+                🟢 <strong>Dentro da expectativa</strong>
+                <br>
+                Pontuação: ${pontuacao}/20
+            </div>
+        `;
+    } else if (pontuacao >= 6) {
+        resultado.innerHTML = `
+            <div class="amarelo">
+                🟡 <strong>Abaixo da expectativa</strong>
+                <br>
+                Pontuação: ${pontuacao}/20
+            </div>
+        `;
+    } else {
+        resultado.innerHTML = `
+            <div class="vermelho">
+                🔴 <strong>Crítico</strong>
+                <br>
+                Pontuação: ${pontuacao}/20
+            </div>
+        `;
+    }
+}
+
+questionario(0);
